204f169ebe96901d0aa8a4ea3875e7124e36fdf4
Guido Asbun <guido@asbun.io>
2021-06-22 20:39:33 -0700

    03_colors

--- 03_colors/sketch.js
+++ 03_colors/sketch.js
@@ -4,23 +4,24 @@
 
 // Example 1-3: RGB Color
 function setup() {
-    createCanvas(displayWidth, displayHeight);
-    noStroke();
+  createCanvas(displayWidth, displayHeight);
+  noStroke();
 }
+
+function draw() {
+  background(255);
+
+  // Bright red -> change my fill!
+  fill(255, 56, 56, 200);
+  ellipse(100, 100, 100, 100);
+
+  // Dark red -> change my fill!
+  fill(127, 0, 0, 200);
+  ellipse(300, 100, 100, 100);
+
+  // Pink (pale red) -> change my fill!
+  fill(255, 200, 200, 255);
+  ellipse(500, 100, 100, 100);
   
-  function draw() {
   
-    background(255);
-    
-    // Bright red -> change my fill!
-    fill(255,0,0);
-    ellipse(100,100,100,100);
-  
-    // Dark red -> change my fill!
-    fill(127,0,0);
-    ellipse(300,100,100,100);
-  
-    // Pink (pale red) -> change my fill!
-    fill(255,200,200);
-    ellipse(500,100,100,100);
-  }
+}
