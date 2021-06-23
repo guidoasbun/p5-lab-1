50d9bb959a537b0045fd370ca23fb12fcf09ef9d
Guido Asbun <guido@asbun.io>
2021-06-22 21:35:20 -0700

    10_gradient

--- 10_gradient/sketch.js
+++ 10_gradient/sketch.js
@@ -1,17 +1,17 @@
 //code snippet adapted from https://editor.p5js.org/juliamaltz/sketches/H1sO_VBSb
-var color1;
-var color2;
+let color1;
+let color2;
 
-function setup() { 
+function setup() {
   createCanvas(windowWidth, windowHeight);
   color1 = color(255, 0, 0);
   color2 = color(0, 0, 255);
-} 
+}
 
-function draw() { 
-  var percentage = map(mouseX, 0, width, 0, 1);
-  var color = lerpColor(color1, color2, percentage);
-  stroke(250);
+function draw() {
+  let percentage = map(mouseY, 0, height, 0, 1);
+  let color = lerpColor(color1, color2, percentage);
+  noStroke();
   fill(color);
-  ellipse(mouseX, mouseY, 50,50);
+  ellipse(mouseX, mouseY, 75, 75);
 }
