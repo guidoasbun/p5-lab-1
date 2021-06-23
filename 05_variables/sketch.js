bf412e44301145def5d290e9fda4872852e7175b
Guido Asbun <guido@asbun.io>
2021-06-22 20:57:10 -0700

    05_variables

--- 05_variables/sketch.js
+++ 05_variables/sketch.js
@@ -1,12 +1,12 @@
 //Code Snippet from Derrick McMillen https://replit.com/@demcrepl/p5js-Spirals?v=1 and // Lauren McCarthy, Casey Reas, Ben Fry
 
 let backgroundcol;
-var y = 60; // 100
-var d = 80; // 130
+let y = 100; // 100
+let d = 130; // 130
 
 function setup() {
     // TRY CHANGING the following variable:
-    backgroundcol = color(245, 224, 66);
+    backgroundcol = color(245, 224, 250);
     createCanvas(600, 600);
     stroke(245, 66, 173);
     noFill();
@@ -14,23 +14,23 @@ function setup() {
 }
 
 
-function draw() {
-  ellipse(75, y, d, d);   // left
-  ellipse(175, y, d, d);  // middle
-  ellipse(275, y, d, d);  // right
-}
-
 // function draw() {
-//     background(backgroundcol);
-//     translate(width / 2, height / 2);
-//     // TRY CHANGING the following three variables:
-//     let length = 1;
-//     let angle = 1;
-//     let loops = 400;
-
-//     for(var i = 0; i < loops; i++) {
-//         rotate(angle);
-//         ellipse(-length/2, -length/2, length, length);
-//         length = length + 1;
-//     }
+//   ellipse(75, y, d, d);   // left
+//   ellipse(175, y, d, d);  // middle
+//   ellipse(275, y, d, d);  // right
 // }
+
+function draw() {
+    background(backgroundcol);
+    translate(width / 2, height / 2);
+    // TRY CHANGING the following three variables:
+    let length = 50;
+    let angle = 30;
+    let loops = 500;
+
+    for(let i = 0; i < loops; i++) {
+        rotate(angle);
+        ellipse(-length/2, -length/2, length, length);
+        length = length + 1;
+    }
+}
