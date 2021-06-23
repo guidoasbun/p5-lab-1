8068aed5b7a597e7d3687971d415ea2d5b4131f4
Guido Asbun <guido@asbun.io>
2021-06-22 21:05:40 -0700

    06_mouseVariables

--- 06_mousevariables/sketch.js
+++ 06_mousevariables/sketch.js
@@ -4,14 +4,20 @@ let backgroundcol;
 let length = 100;
 
 function setup() {
-    backgroundcol = color(245, 224, 66);
-    createCanvas(600, 600);
-    stroke(245, 66, 173);
-    noFill();
-    background(backgroundcol);
+  backgroundcol = color(245, 224, 66);
+  createCanvas(displayWidth, displayHeight);
+  stroke(245, 66, 173);
+  noFill();
+  background(backgroundcol);
 }
 
 function draw() {
-    background(backgroundcol);
-    ellipse(20, 30, length, length);
+//   background(backgroundcol);
+  if (mouseIsPressed) {
+    stroke("red");
+  } else {
+    stroke("green");
+  }
+  ellipse(mouseX, mouseY, length, length);
+  console.log(`mouseX = ${mouseX}, mouseY = ${mouseY}`);
 }
