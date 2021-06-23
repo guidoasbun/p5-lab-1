927b4fc6fc1d88e87bf1b19514336a54d1803d54
Guido Asbun <guido@asbun.io>
2021-06-22 21:54:21 -0700

    11_images

--- 11_images/sketch.js
+++ 11_images/sketch.js
@@ -1,17 +1,39 @@
 //code snippet adapted from https://editor.p5js.org/juliamaltz/sketches/H1sO_VBSb
-var color1;
-var color2;
+// var color1;
+// var color2;
 
-function setup() { 
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   color1 = color(255, 0, 0);
+//   color2 = color(0, 0, 255);
+// }
+
+// function draw() {
+//   var percentage = map(mouseX, 0, width, 0, 1);
+//   var color = lerpColor(color1, color2, percentage);
+//   stroke(250);
+//   fill(color);
+//   ellipse(mouseX, mouseY, 50,50);
+// }
+
+
+let img;
+function preload() {
+  img = loadImage('banana.png');
+}
+
+function setup() {
   createCanvas(windowWidth, windowHeight);
-  color1 = color(255, 0, 0);
-  color2 = color(0, 0, 255);
-} 
+  
+}
 
-function draw() { 
-  var percentage = map(mouseX, 0, width, 0, 1);
-  var color = lerpColor(color1, color2, percentage);
-  stroke(250);
-  fill(color);
-  ellipse(mouseX, mouseY, 50,50);
+function draw() {
+  noStroke();
+  // if (mouseIsPressed) {
+  //   fill("white");
+  // } else {
+  //   fill("teal");
+  // }
+ image(img, mouseX, mouseY, 100, 100);
+  // ellipse(mouseX, mouseY, 300, 300);
 }
